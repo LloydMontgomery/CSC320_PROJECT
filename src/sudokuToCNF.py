b60cc87db5c86f360963691df109a67ea9f73805
Lloyd Montgomery <lloydrmontgomery@gmail.com>
2015-03-28 22:21:22 -0700

    Lookup table done

--- src/sudokuToCNF.py
+++ src/sudokuToCNF.py
@@ -2,15 +2,46 @@
 import sys
 import helperLibrary
 
+sudokuFile = 0
+sodukuLookupTable = [0]*729
+
 def initFileHandles():
-	sudoku = open("foo1.txt", "w+")
-	print (fo.name)
-	fo.close()
+	global sudokuFile
+	if len(sys.argv) == 1:
+		print "Missing a command-line argument: filename of input sudoku problem"
+		return
+	sudokuFile = open(sys.argv[1], "r")
+
+def closeFileHandles():
+	global sudokuFile
+	sudokuFile.close()
 
 def convertSudoku():
-	pass
+	global sudokuFile
+	sudoku = sudokuFile.readline()
+	if len(sudoku) != 81:
+		print "Sudoku file is of incorrect format"
+
+	x = y = 1
+	for i in range(0, 81):
+		if sudoku[i].isdigit() and int(sudoku[i]) > 0:
+			index = helperLibrary.xyzToBase9(x, y, int(sudoku[i]))
+			sodukuLookupTable[index] = 1
+		x += 1
+		if x == 10:
+			x = 1
+			y += 1
+
+
+	print sodukuLookupTable[4]
+
+
 
 def main():
 	initFileHandles()
+	convertSudoku()
+
+
+	closeFileHandles()
 	
 main()
